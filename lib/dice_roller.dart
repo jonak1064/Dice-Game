ddd5d5bfd6fdf6bada5689fd3217ce563043c86d
jonak1064 <124136656+jonak1064@users.noreply.github.com>
2024-04-29 13:04:48 +0600

    upload

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,6 +1,7 @@
 //import 'dart:html';
 
-import 'package:dice_game/rounds.dart';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -20,12 +21,15 @@ class DiceRoller extends StatefulWidget {
 }
 
 final randomizer = Random();
-//Widget topText1 = const Text(' ');
-//Widget topText2 = const Text(' ');
+Widget result1 = const Text(' ');
+Widget result2 = const Text(' ');
+Widget result3 = const Text(' ');
 
 var finalScore1 = 0;
 var finalScore2 = 0;
 
+var rollCount = 5;
+var roundCount = 1;
 Widget finalResult = const Text(' ');
 
 class _DiceRollerState extends State<DiceRoller> {
@@ -33,30 +37,56 @@ class _DiceRollerState extends State<DiceRoller> {
   var currentDiceRoll2 = 1;
 
   void rollDice() {
-    setState(() {
-      currentDiceRoll1 = randomizer.nextInt(6) + 1;
-      currentDiceRoll2 = randomizer.nextInt(6) + 1;
-      finalScore1 = 0;
-      finalScore2 = 0;
-      finalScore1 = finalScore1 + currentDiceRoll1;
-      finalScore2 = finalScore2 + currentDiceRoll2;
+    if (roundCount < rollCount) {
+      setState(() {
+        currentDiceRoll1 = randomizer.nextInt(6) + 1;
+        currentDiceRoll2 = randomizer.nextInt(6) + 1;
+        roundCount++;
+        finalScore1 = finalScore1 + currentDiceRoll1;
+        finalScore2 = finalScore2 + currentDiceRoll2;
+      });
+    } else {
+      setState(() {
+        finalResult = const Text('Game Over');
 
-      if (finalScore1 > finalScore2) {
-        finalResult = const Text(
-          'Player 1 wins the game',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        );
-      } else if (finalScore1 < finalScore2) {
-        finalResult = const Text(
-          'Player 2 wins the game',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        );
-      } else {
-        finalResult = const Text(
-          'Its a tie!',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        );
-      }
+        if (finalScore1 > finalScore2) {
+          setState(() {
+            result1 = const Text('Player 1 wins');
+            result2 = Text(finalScore1.toString());
+            result3 = Text(finalScore2.toString());
+          });
+        } else if (finalScore1 < finalScore2) {
+          setState(() {
+            result1 = const Text('Player 2 wins');
+            result2 = Text(finalScore1.toString());
+            result3 = Text(finalScore2.toString());
+          });
+        } else {
+          setState(() {
+            result1 = const Text('its a tie!');
+            result2 = Text(finalScore1.toString());
+            result3 = Text(finalScore2.toString());
+          });
+        }
+      });
+    }
+  }
+
+  void fiveTimes() {
+    setState(() {
+      rollCount = 5;
+    });
+  }
+
+  void tenTimes() {
+    setState(() {
+      rollCount = 10;
+    });
+  }
+
+  void twentyTimes() {
+    setState(() {
+      rollCount = 20;
     });
   }
 
@@ -75,7 +105,40 @@ class _DiceRollerState extends State<DiceRoller> {
           const SizedBox(height: 40),
           const Text('How many rounds do you want to play?'),
           const SizedBox(height: 30),
-          const Rounds(),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: fiveTimes,
+                style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 101, 65, 65),
+                    textStyle: const TextStyle(
+                      fontSize: 24.0,
+                      decorationThickness: BorderSide.strokeAlignCenter,
+                    )),
+                child: const Text('5'),
+              ),
+              TextButton(
+                onPressed: tenTimes,
+                style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 101, 65, 65),
+                    textStyle: const TextStyle(
+                      fontSize: 24.0,
+                    )),
+                child: const Text('10'),
+              ),
+              TextButton(
+                onPressed: twentyTimes,
+                style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 101, 65, 65),
+                    textStyle: const TextStyle(
+                      fontSize: 24.0,
+                    )),
+                child: const Text('20'),
+              ),
+            ],
+          ),
           const SizedBox(height: 40),
           TextButton(
             onPressed: rollDice,
@@ -87,7 +150,11 @@ class _DiceRollerState extends State<DiceRoller> {
             child: const Text('Roll'),
           ),
           const SizedBox(height: 20),
+          result1,
           finalResult,
+          result2,
+          const SizedBox(height: 10),
+          result3,
         ],
       ),
     );
